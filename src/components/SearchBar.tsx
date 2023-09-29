0d21b198b6476cf784085c39e9797eb5e1675d12
Duzaoex <eduardoneriscarneiro@gmail.com>
2023-09-29 14:10:24 -0300

    iniciando

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,38 @@ import React from 'react';
 function SearchBar() {
   return (
     <div>
-      <input data-testid="search-input" type="text" />
+      <input data-testid="search-input" type="text" placeholder="Digite sua busca..." />
+      <div>
+        <label>
+          <input
+            type="radio"
+            data-testid="ingredient-search-radio"
+            name="search-type"
+          />
+          Buscar por Ingrediente
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            data-testid="name-search-radio"
+            name="search-type"
+          />
+          Buscar por Nome
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            data-testid="first-letter-search-radio"
+            name="search-type"
+          />
+          Buscar pela Primeira Letra
+        </label>
+      </div>
+      <button data-testid="exec-search-btn">Buscar</button>
     </div>
   );
 }
