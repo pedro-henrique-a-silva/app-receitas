c8f96bd9ea05a07928a935b4369992969ca3a3ce
Josue Azevedo <josuerodriguers03@gmail.com>
2023-09-29 13:06:40 -0300

    refatore

--- src/Pages/FavoriteRecipes.tsx
+++ src/Pages/FavoriteRecipes.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 import Profile from './Profile';
 
 function FavoriteRecipes() {
   return (
-    <>
-      <Header title="Favorite Recipes" isProfile isSearch={ false } />
-      <Profile />
-    </>
+    <Profile />
   );
 }
 
